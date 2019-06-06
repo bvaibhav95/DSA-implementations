f2a30735cad92d27f8b3aefc81cd3a110fd6baa6
bvaibhav95 <bvaibhav.95@gmail.com>
2019-06-06 11:08:55 +0530

    Stack impl

--- javascript/Impl/stack.js
+++ javascript/Impl/stack.js
@@ -1,4 +1,5 @@
-// Implement 2 Stacks using an array of fixed length
+// Impl : 1
+//Implement 2 Stacks using an array of fixed length
 /*
 1. Divide into half and once pointer for Stack1 goes above half or pointer for Stack2 goes below half...StackOverflow
 2. Start pushing into 2 stacks from different ends of the array, when pointers meet each other make stack overflow
@@ -52,17 +53,28 @@ class Stack2 {
   }
 }
 
-let arr = Array(5);
-let s1 = new Stack1(arr);
-let s2 = new Stack2(arr);
-s1.push(1);
-s1.push(1);
-s1.push(1);
-s1.pop();
-s2.push(1);
-s2.push(1);
-s2.push(1);
-s2.push(1);
-s1.push(1);
-s2.pop();
-console.log(arr);
+// let arr = Array(5);
+// let s1 = new Stack1(arr);
+// let s2 = new Stack2(arr);
+// s1.push(1);
+// s1.push(1);
+// s1.push(1);
+// s1.pop();
+// s2.push(1);
+// s2.push(1);
+// s2.push(1);
+// s2.push(1);
+// s1.push(1);
+// s2.pop();
+// console.log(arr);
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+//Impl : 2
+/**
+ * How to implement a stack which will support following operations in O(1) time complexity?
+ * 1) push() which adds an element to the top of stack.
+ * 2) pop() which removes an element from top of stack.
+ * 3) findMiddle() which will return middle element of the stack.
+ * 4) deleteMiddle() which will delete the middle element.
+ */
