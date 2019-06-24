a8b01400e33098b4172578bb8b4c46ed8b201a1b
bvaibhav95 <bvaibhav.95@gmail.com>
2019-06-24 17:27:13 +0530

    RedBlackTree

---
+++ javascript/RedBlackTree.js
@@ -0,0 +1,23 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class RedBlackTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {}
+  search(value) {}
+  delete(value) {}
+}
+
+class RedBlackTree1 {
+  constructor() {
+    this.left = null;
+    this.right = null;
+  }
+}
