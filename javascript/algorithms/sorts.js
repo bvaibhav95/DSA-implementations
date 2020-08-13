39a092261d786daaa75c9f99965a11f1a22ed571
bvaibhav95 <bvaibhav.95@gmail.com>
2020-08-13 18:59:24 +0530

    name changes

---
+++ javascript/algorithms/sorts.js
@@ -0,0 +1,97 @@
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+function bubbleSortWithShortCircuit(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let flag = false;
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        flag = true;
+      }
+    }
+    if (!flag) {
+      break;
+    }
+  }
+  return arr;
+}
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[min] > arr[j]) {
+        min = j;
+      }
+    }
+    if (i !== min) {
+      console.log(i, min);
+      let temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp;
+    }
+  }
+  return arr;
+}
+
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currentVal = arr[i];
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = currentVal;
+  }
+  return arr;
+}
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
+
+function merge(arr1, arr2) {
+  let arr = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] > arr2[j]) {
+      arr.push(arr2[j]);
+      j++;
+    } else {
+      arr.push(arr1[i]);
+      i++;
+    }
+  }
+  while (i < arr1.length) {
+    arr.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    arr.push(arr2[j]);
+    j++;
+  }
+  return arr;
+}
+
+function quickSort(arr) {}
+
+function partition(params) {}
+
+console.log(mergeSort([-5, 1, 0, -1, 10, 10, 17]));
